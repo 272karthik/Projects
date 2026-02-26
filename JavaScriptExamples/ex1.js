7d56ff853e4a46708170e9e8f8cf16149ce4ca90
272karthik <133763783+272karthik@users.noreply.github.com>
2026-02-26 12:10:28 +0530

    Add files via upload

---
+++ JavaScriptExamples/ex1.js
@@ -0,0 +1,3 @@
+var tweet = prompt("compose your tweet : ");
+var tweetCount = tweet.length;
+console.log("you have written " + tweetCount + " characters, you have " + (280 - tweetCount) + " characters length remaining");
